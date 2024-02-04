dfcbe582f3ef807ceadef23dc1357e08d0f2f134
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-02-04 17:27:52 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -28,3 +28,9 @@ input.onButtonPressed(Button.B, function () {
 })
 MotorDriver.ServoTurnAngle(Servo.S0, 0)
 MotorDriver.ServoTurnAngle(Servo.S1, 0)
+basic.pause(2000)
+MotorDriver.ServoStop(Servo.S0)
+MotorDriver.ServoStop(Servo.S1)
+basic.forever(function () {
+	
+})
