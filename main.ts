dd7afc7a94167baca5b54f493db67e026dc5091e
digidagsOrg <107912351+digidagsOrg@users.noreply.github.com>
2024-02-05 17:57:58 +0100

    Aktualisiere main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,17 +1,3 @@
-input.onButtonPressed(Button.A, function () {
-    basic.showLeds(`
-        . . . . .
-        # . . . #
-        # . . . #
-        . # # # .
-        . . . . .
-        `)
-    MotorDriver.ServoTurnAngle(Servo.S0, 0)
-    MotorDriver.ServoTurnAngle(Servo.S1, 0)
-    basic.pause(2000)
-    MotorDriver.ServoStop(Servo.S0)
-    MotorDriver.ServoStop(Servo.S1)
-})
 input.onButtonPressed(Button.B, function () {
     basic.showLeds(`
         . # # # .
@@ -26,12 +12,44 @@ input.onButtonPressed(Button.B, function () {
     MotorDriver.ServoStop(Servo.S0)
     MotorDriver.ServoStop(Servo.S1)
 })
-MotorDriver.ServoTurnAngle(Servo.S0, 0)
-MotorDriver.ServoTurnAngle(Servo.S1, 0)
-input.calibrateCompass()
+function servo_rotate (num: number, num2: number) {
+    rand = randint(0, 1)
+    if (rand == 1) {
+        MotorDriver.ServoTurnAngle(Servo.S0, num2)
+        MotorDriver.ServoTurnAngle(Servo.S1, num)
+    } else {
+        MotorDriver.ServoTurnAngle(Servo.S0, num)
+        MotorDriver.ServoTurnAngle(Servo.S1, num2)
+    }
+}
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+        . . . . .
+        # . . . #
+        # . . . #
+        . # # # .
+        . . . . .
+        `)
+    MotorDriver.ServoTurnAngle(Servo.S0, 0)
+    MotorDriver.ServoTurnAngle(Servo.S1, 0)
+    basic.pause(2000)
+    MotorDriver.ServoStop(Servo.S0)
+    MotorDriver.ServoStop(Servo.S1)
+})
+let rand = 0
+MotorDriver.ServoTurnAngle(Servo.S0, 90)
+MotorDriver.ServoTurnAngle(Servo.S1, 90)
 basic.pause(2000)
 MotorDriver.ServoStop(Servo.S0)
 MotorDriver.ServoStop(Servo.S1)
+basic.showIcon(IconNames.SmallSquare)
 basic.forever(function () {
-    basic.showNumber(input.rotation(Rotation.Pitch))
+    if (input.acceleration(Dimension.Strength) > 1070) {
+        basic.showIcon(IconNames.SmallHeart)
+        servo_rotate(50, 130)
+        basic.pause(2000)
+        servo_rotate(90, 90)
+        basic.showIcon(IconNames.SmallSquare)
+        basic.pause(2000)
+    }
 })
